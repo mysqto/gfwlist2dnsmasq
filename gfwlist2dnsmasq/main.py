6af4300356ffa4c945ebbba3adffd9f4d416781d
Chen Lei <my@mysq.to>
2016-01-01 09:58:24 +0800

    update with domain reduce from gfwlist2pac

--- gfwlist2dnsmasq/main.py
+++ gfwlist2dnsmasq/main.py
@@ -28,6 +28,19 @@ import sys
 import socket
 import re
 from argparse import ArgumentParser
+try:
+	from urllib.request import urlopen
+except ImportError:
+	from urllib2 import urlopen
+try:
+	from urllib.parse import urlparse
+except ImportError:
+	from urlparse import urlparse
+
+try:
+	xrange
+except NameError:
+	xrange = range
 
 __all__ = ['main']
 
@@ -35,36 +48,42 @@ gfwlist_url = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('-i', '--input', dest='input', required=False,
-            help='path to gfwlist file, raw url or local path,'\
+    parser.add_argument('-i', '--input', dest = 'input', required = False,
+            help = 'path to gfwlist file, raw url or local path,'\
                     ' default will get from gfwlist github repo',
-            metavar='gfwlist.txt')
-    parser.add_argument('-o', '--output', dest='output', required=False,
-            help='path to output dnsmasq.conf, default will write to'\
-                    '  dnsmasq.conf in current directory',
-            default = './dnsmasq.conf', metavar='dnsmasq.conf')
-    parser.add_argument('-s', '--server', dest='server', required=False,
-            help='The upstream dns server address for the poisoned domain,'\
+            metavar = 'gfwlist.txt')
+    parser.add_argument('-u', '--user-rule', dest = 'user_rule', required = False,
+            help = 'customized user rule, which will be append to gfwlist'\
+					' raw url or local path',
+            metavar = 'user_rule.txt')
+    parser.add_argument('-o', '--output', dest = 'output', required = False,
+            help = 'path to output dnsmasq.conf, default will write to'\
+                    '  dnsmasq.gfwlist.conf in current directory',
+            default = './dnsmasq.gfwlist.conf', metavar = 'dnsmasq.conf')
+    parser.add_argument('-s', '--server', dest = 'server', required = False,
+            help = 'The upstream dns server address for the poisoned domain,'\
                     ' default value is 127.0.0.1',
-            default='127.0.0.1', metavar='server')
-    parser.add_argument('-p', '--port', dest='port', required=False,
-            type=int, choices=range(1, 65535), default=5353,
-            help='The upstream dns server port for the poisoned domain,'\
+            default = '127.0.0.1', metavar = 'server')
+    parser.add_argument('-p', '--port', dest = 'port', required = False,
+            type = int, choices = range(1, 65535), default = 5353,
+            help = 'The upstream dns server port for the poisoned domain,'\
                     ' default value is 5353',
-            metavar='port')
-    parser.add_argument('-e', '--ipset', dest='ipset', required=False,
-            help='ipset name of the dnsmasq ipset, ipset not support if'\
+            metavar = 'port')
+    parser.add_argument('-e', '--ipset', dest = 'ipset', required = False,
+            help = 'ipset name of the dnsmasq ipset, ipset not support if'\
                     ' not presented',
-            metavar='ipsetname')
+            metavar = 'ipsetname')
     return parser.parse_args()
 
 
 def decode_gfwlist(content):
-    # decode base64 if have to
-    try:
-        return base64.b64decode(content)
-    except:
-        return content
+	# decode base64 if have to
+	try:
+		if '.' in content:
+			raise Exception()
+		return base64.b64decode(content)
+	except:
+		return content
 
 
 def get_hostname(something):
@@ -76,8 +95,8 @@ def get_hostname(something):
             from urllib.parse import urlparse
         except ImportError:
             from urlparse import urlparse
-        r = urlparse(something)
-        return r.hostname
+        url = urlparse(something)
+        return url.hostname
     except Exception as e:
         logging.error(e) 
         return None
@@ -108,10 +127,6 @@ def is_valid_domain(domain):
 def add_domain_to_set(s, something):
     hostname = get_hostname(something)
     if hostname is not None:
-        if hostname.startswith('.'):
-            hostname = hostname.lstrip('.')
-        if hostname.endswith('/'):
-            hostname = hostname.rstrip('/')
         if is_valid_ipv4_address(hostname):
             return
         if not is_valid_domain(hostname):
@@ -119,39 +134,70 @@ def add_domain_to_set(s, something):
         if hostname:
             s.add(hostname)
 
+def combine_rules(content, user_rule = None):
+	builtin_rules = pkgutil.get_data('gfwlist2dnsmasq', 
+							'resources/builtin.txt').decode().splitlines(False)
+	gfwlist = content.decode().splitlines(False)
+	gfwlist.extend(builtin_rules)
 
-def parse_gfwlist(content):
-    builtin_rules = pkgutil.get_data('gfwlist2dnsmasq', 'resources/builtin.txt').decode().splitlines(False)
-    gfwlist = content.decode().splitlines(False)
-    domains = set(builtin_rules)
-    for line in gfwlist:
-        if line.find('.*') >= 0:
-            continue
-        elif line.find('*') >= 0:
-            line = line.replace('*', '/')
-        if line.startswith('!'):
-            continue
-        elif line.startswith('['):
-            continue
-        elif line.startswith('@'):
-            # ignore white list
-            continue
-        elif line.startswith('||'):
-            add_domain_to_set(domains, line.lstrip('||'))
-        elif line.startswith('|'):
-            add_domain_to_set(domains, line.lstrip('|'))
-        elif line.startswith('.'):
-            add_domain_to_set(domains, line.lstrip('.'))
-        else:
-            add_domain_to_set(domains, line)
-    return domains
+	if user_rule:
+		gfwlist.extend(user_rule.decode().splitlines(False))
+
+	return gfwlist
+
+def parse_gfwlist(gfwlist):
+	domains = set()
+	for line in gfwlist:
+		if line.find('.*') >= 0:
+			continue
+		elif line.find('*') >= 0:
+			line = line.replace('*', '/')
+		if line.startswith('||'):
+			line = line.lstrip('||')
+		elif line.startswith('|'):
+			line = line.lstrip('|')
+		elif line.startswith('.'):
+			line = line.lstrip('.')
+		if line.startswith('!'):
+			continue
+		elif line.startswith('['):
+			continue
+		elif line.startswith('@'):
+			# ignore white list
+			continue
+		add_domain_to_set(domains, line)
+
+	return domains
+
+def reduce_domains(domains):
+	# reduce 'www.google.com' to 'google.com'
+	# remove invalid domains
+	tld_content = pkgutil.get_data('gfwlist2dnsmasq', 'resources/tld.txt').decode()
+	tlds = set(tld_content.splitlines(False))
+	new_domains = set()
+	for domain in domains:
+		domain_parts = domain.split('.')
+		last_root_domain = None
+		for i in xrange(0, len(domain_parts)):
+			root_domain = '.'.join(domain_parts[len(domain_parts) - i - 1:])
+			if i == 0:
+				if not tlds.__contains__(root_domain):
+					# root_domain is not a valid tld
+					break
+			last_root_domain = root_domain
+			if tlds.__contains__(root_domain):
+				continue
+			else:
+				break
+		if last_root_domain is not None:
+			new_domains.add(last_root_domain)
+	return sorted(new_domains)
 
 def generate_dnsmasq(domains, args):
     with open(args.output, 'w') as of:
         of.write('# gfwlist dnsmasq rule generated by gfwlist2dnsmasq\n')
         of.write('# updated on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
         of.write('# bug report: https://github.com/mysqto/gfwlist2dnsmasq\n\n')
-        domains = sorted(domains)
         for domain in domains:
             if args.port == 53:
                 of.write('server=/.%s/%s\n'%(domain, args.server))
@@ -162,25 +208,42 @@ def generate_dnsmasq(domains, args):
     of.close()
 
 def main():
-    args = parse_args()
-    try:
-        with open(args.input, 'rb') as f:
-            content = f.read()
-    except:
-        try:
-            try:
-                from urllib.request import urlopen
-            except ImportError:
-                from urllib2 import urlopen
-            content = urlopen(gfwlist_url, timeout = 15).read()
-        except:
-            import traceback            
-            print(traceback.format_exc())
-            print('Error, cannot download gfwlist from repo, please specify one.')
-            sys.exit(-1)
-    content = decode_gfwlist(content)
-    domains = parse_gfwlist(content)
-    generate_dnsmasq(domains, args)
+	args = parse_args()
+	content = None
+	user_rule = None
+
+	if (args.input):
+		gfwlist_parts = urlsplit(args.input)
+		
+		if not gfwlist_parts.scheme or not gfwlist_parts.netloc:
+			# It's not an URL, local file
+			with open(args.input, 'rb') as f:
+				content = f.read()
+		else:
+			print('Downloading gfwlist from ', gfwlist_url)
+			content = urlopen(gfwlist_url, timeout = 15).read()
+	
+	else:
+		print('Downloading gfwlist from ', gfwlist_url)
+		content = urlopen(gfwlist_url, timeout = 15).read()
+
+	if args.user_rule:
+		userrule_parts = urlsplit(args.user_rule)
+		
+		if not userrule_parts.scheme or not userrule_parts.netloc:
+			# It's not an URL, local file
+			with open(args.user_rule, 'rb') as f:
+				user_rule = f.read()
+		else:
+			# Yeah, it's an URL, try to download it
+			print('Downloading user rules file from ', args.user_rule)
+			user_rule = urlopen(args.user_rule, timeout = 15).read()
+
+	content = decode_gfwlist(content.decode())
+	gfwlist = combine_rules(content, user_rule)
+	domains = parse_gfwlist(gfwlist)
+	domains = reduce_domains(domains)
+	generate_dnsmasq(domains, args)
         
 if __name__ == '__main__':
     main()
